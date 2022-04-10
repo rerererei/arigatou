8622236d42859b07efa00154aef55fbc66d70832
rei <ishibababa@gmail.com>
2022-04-10 16:11:10 +0900

    mac commit

--- rwiz-jp.com_1/page-recruiting.php
+++ rwiz-jp.com_1/page-recruiting.php
@@ -22,7 +22,7 @@
     <div class="top-content">
         <div style="font-weight: 700;font-size: 2.2vw;line-height: 5vw;">横須賀市・三浦市の訪問介護</div>
         <div style="font-weight: 700;font-size: 4.2vw;line-height: 5vw;">登録ヘルパーで自由な働き方を</div>
-        <div style="font-weight: 400;font-size: 2.2vw;line-height: 5vw;">夜勤なし・シフト制で<span style="font-weight: 800;font-size: 3vw;line-height: 5vw;color:#7CC6AB">時給1,600円</span>から</div>
+        <div style="font-weight: 400;font-size: 2.2vw;line-height: 5vw;">夜勤なし・シフト制で<span style="font-weight: 800;font-size: 3vw;line-height: 5vw;color:#7CC6AB;">時給<span style="font-size:1.3em;font-weight: 400;">1,600</span>円</span>から</div>
         <div class="btn mt-5" style="width: 25vw;height: 4vw;border-radius: 100px;background-color:#FF8D23;font-weight: 700;font-size: 2vw;color:#FFF">お問い合わせ</div>
     </div>  
 </div>
@@ -32,7 +32,49 @@
 <!-- Problems-->
 <div class="problems-area">
     <h2>Problems</h2>
-
+    <div class="text-center lp-caption">こんなお悩みはありませんか？</div>
+    <div class="row text-center problem-row" style="width:85%;margin: 2vw auto;">
+        <div class="col-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">職場の人間関係に疲れた<br></h5>
+                    <div class="problem-card-image">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/problem-personal-connect.png" class="card-img-top">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">給与・バイト代が低い<br></h5>
+                    <div class="problem-card-image">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/problem-money.png" class="card-img-top">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">人手不足で毎日忙しい<br></h5>
+                    <div class="problem-card-image">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/problem-busy.png" class="card-img-top">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">働きたい時間だけだと<br>時間がない</h5>
+                    <div class="problem-card-image">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/problem-calender.png" class="card-img-top">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 
