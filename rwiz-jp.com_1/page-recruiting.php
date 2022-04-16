43b69570eaf42ec4d452ce05b1028c40543ca473
ishibashi <ishibashi@fubright.co.jp>
2022-04-16 19:06:06 +0900

    voiceのところまで完成

--- rwiz-jp.com_1/page-recruiting.php
+++ rwiz-jp.com_1/page-recruiting.php
@@ -31,7 +31,7 @@
 <div class="top-drawer"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/drawer.png"></div>
 <!-- Problems-->
 <div class="problems-area">
-    <h2>Problems</h2>
+    <h2>problems</h2>
     <div class="text-center lp-caption">こんなお悩みはありませんか？</div>
     <div class="row text-center problem-row" style="width:85%;margin: 2vw auto;">
         <div class="col-3">
@@ -74,6 +74,88 @@
                 </div>
             </div>
         </div>
+    </div>
+</div>
+<!-- features-->
+<div class="features-area">
+    <div style="position: relative;">
+        <div style="margin-bottom: -8vw;"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/tryangle.png" style="width: 100%;"></div>
+        <div class="text-center lp-caption fature-caption">『訪問介護のありがとう』なら..</div>
+    </div>
+    <h2>features</h2>
+    <div class="text-center mt-3 mb-3" style="font-size:3vw;font-weight: 700;">働きやすいから選ばれる！３つの特徴</div>
+    <div class="row bg-white" style="width:75%;margin: 2vw auto;margin-bottom: 0.3vw;border-radius: 1vw 1vw 0 0;">
+        <div class="col-8" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/feature-num1.png);background-position-x: 3vw;background-position-y: 1vw;background-size: 8vw;">
+            <div class="feature-title">働きたい時間に、働きたいだけ</div>
+            <div class="feature-sentence">週１回・１時間からシフト設定ができ、自分の生活リズムにあわせて働くことができます。家事・育児のスキマ時間に働いたり、別の仕事との両立が可能です。始業時間は訪問先へ直行し、就業後は自宅へ直帰もOK。</div>
+        </div>
+        <div class="col-4 p-3" >
+            <img class="w-100" src="<?php echo get_stylesheet_directory_uri(); ?>/img/feature1.png">
+        </div>
+    </div>
+    <div class="row problem-row bg-white" style="width:75%;margin: 0 auto;margin-bottom: 0.3vw;border-radius: 0;">
+        <div class="col-4 p-3" >
+            <img class="w-100" src="<?php echo get_stylesheet_directory_uri(); ?>/img/feature2.png">
+        </div>
+        <div class="col-8" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/feature-num2.png);background-position-x: 35vw;background-position-y: 2vw;background-size: 8vw;">
+            <div class="feature-title" style="margin-left: 0;">夜勤なし・高い給与水準</div>
+            <div class="feature-sentence" style="margin-left: 0;width: 40vw;">訪問介護ありがとうは、夜勤なし・高い給与水準など、地域一働きやすい会社を目指しています。利用者さまからいただく「ありがとう」を、ご登録いただいているヘルパーさんの給与面や資格取得に反映しております。</div>
+        </div>
+    </div>
+    <div class="row problem-row bg-white" style="width:75%;margin: 2vw auto;margin-top: 0.3vw;border-radius: 0 0 1vw 1vw;">
+        <div class="col-8" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/feature-num3.png);background-position-x: 3vw;background-position-y: 2vw;background-size: 8vw;">
+            <div class="feature-title" style="margin-top: 2vw;">資格取得の費用は全額支援</div>
+            <div class="feature-sentence">これから介護業界を目指す方や訪問介護が未経験の方などでもご安心ください。介護業務に必要な「介護職員初任者研修」やその他の資格を取得する際には、資格取得に必要な費用を全額支援しており、キャリアアップしやすい環境づくりをしています。</div>
+            <div class="feature-sentence" style="font-size: 0.9vw;">※お仕事の紹介までサポートさせていただき、一定の勤務時間を終えられた方のみの支援となります。</div>
+        </div>
+        <div class="col-4 p-3" >
+            <img class="w-100" src="<?php echo get_stylesheet_directory_uri(); ?>/img/feature3.png">
+        </div>
+    </div>
+</div>
+<!-- what home visit care-->
+<div class="whvc-area">
+    <div>
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/wave-top.png" style="width: 100%;">
+    </div>
+    <h2>what home visit care</h2>
+    <div class="text-center mt-3 mb-3" style="font-size:3vw;font-weight: 700;">訪問介護ってどんなことをするの？</div>
+    <div class="row bg-white" style="width:75%;margin: 2vw auto;margin-bottom: 0.3vw;border-radius: 1vw 1vw 0 0;">
+        <div class="col-6" style="height: 39vw;">
+            <div class="whvc-title">身体介護</div>
+            <div class="whvc-img">
+                <img class="w-100" src="<?php echo get_stylesheet_directory_uri(); ?>/img/whvc-img1.png">
+            </div>
+            <div class="whvc-caption">入浴や排泄の介助など、<br>お身体にかかわる介護を中心に行います。</div>
+            <div class="whvc-sentence">食事介助・排泄介助・おむつ交換・体位交換・衣類着脱・入浴介助・清拭・手、脚浴・通院介助・服薬介助・整容（爪切り・髭剃り、その他）など</div>
+            <div class="whvc-square"></div>
+        </div>
+        <div class="col-6" style="height: 39vw;">
+            <div class="whvc-title btn-info">生活援助</div>
+            <div class="whvc-img">
+                <img class="w-100" src="<?php echo get_stylesheet_directory_uri(); ?>/img/whvc-img2.png">
+            </div>
+            <div class="whvc-caption">掃除/洗濯/調理など生活にかかわるお手伝いや、<br>薬の受け取りの代行なども行います。</div>
+            <div class="whvc-sentence">配膳・後片付け・布団干し・シーツ交換・衣類の補修・住居の掃除・ゴミ出し・生活必需品の買い物・換気・採光・火の始末など。</div>
+            <div class="whvc-square"></div>
+        </div>
+    </div>
+    <div class="whvc-last-sentence">利用者さまの自宅を訪問し、介護業務を行うため<br>1人1人としっかり向き合えるお仕事です。</div>
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/wave-under.png" style="width: 100%;margin-top:4vw;">
+</div>
+<!-- voice-->
+<div class="voice-area">
+    <h2>voice</h2>
+    <div class="text-center mt-3 mb-5" style="font-size:3vw;font-weight: 700;">実際に働いているスタッフの声</div>
+    <div class="w-50 staff-1" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/voice-bg1.png);">
+        <img class="staff-img" src="<?php echo get_stylesheet_directory_uri(); ?>/img/voice-1_2.png">
+        <div class="staff-name">有光 貴美子さん</div>
+        <div class="staff-age">経験年数12年</div>
+        <div class="staff-title">同じ業務内容で給与が1.5倍に！<br>新しいことへのチャレンジも<br>できる環境です</div>
+        <div class="staff-sentence">今まで自分の行なっていたことを高く会社に評価してもらい、業務内容は変わらないのに給与が1.5倍以上になりました♪<br>その中で経験を活かしてもう一段上のことにもチャレンジできるので新しいステップアップの形が見えました！！</div>
+    </div>
+    <div class="w-50 staff-2" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/voice-bg2.png);">
+        <img class="staff-img" src="<?php echo get_stylesheet_directory_uri(); ?>/img/voice-2_2.png">
     </div>
 </div>
 
